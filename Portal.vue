c14b3bbb26ea5e5935460484e259114028a2b623
Ingrid Lorentz <60070476+ingridlorentz@users.noreply.github.com>
2021-05-06 11:55:20 -0500

    files

---
+++ Portal.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="vue-portal">
+  <b-jumbotron>
+    <template #header>UI VUE</template>
+
+    <template #lead>
+      Bienvenido al portal Vue
+    </template>
+
+    <hr class="my-4">
+
+    <p>
+     Ingrese a su perfil para acceder a su información. 
+    </p>
+
+    <b-button variant="primary" href="#">Contacto</b-button>
+    <b-button variant="success" href="#">Ingresar</b-button>
+  </b-jumbotron>
+</div>
+</template>
